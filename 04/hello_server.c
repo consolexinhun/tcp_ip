590a2afe3af0712fe42757257e507dbacad9bfa0
consolexin <consolexin@gmail.com>
2022-07-13 14:06:55 +0800

    echo server client

--- 04/hello_server.c
+++ 04/hello_server.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #include "error_handle.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int serv_sock;
     int client_sock;
 
@@ -17,14 +18,16 @@ int main(int argc, char *argv[]) {
 
     char message[] = "hello world!\n";
 
-    if(argc != 2) {
+    if (argc != 2)
+    {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
     // 创建服务端套接字
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(serv_sock == -1) {
+    if (serv_sock == -1)
+    {
         error_handling("socket() error");
     }
 
@@ -33,16 +36,19 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if(bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    {
         error_handling("bing() error");
     }
-    if(listen(serv_sock, 5) == -1) {
+    if (listen(serv_sock, 5) == -1)
+    {
         error_handling("listen() error");
     }
 
     client_addr_size = sizeof(client_addr);
     client_sock = accept(serv_sock, (struct sockaddr *)&client_addr, &client_addr_size);
-    if(client_sock == -1) {
+    if (client_sock == -1)
+    {
         error_handling("accept() error");
     }
 
