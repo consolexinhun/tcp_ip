806de758f4d9c2e6f5f222d9b379278bd16a5beb
consolexin <consolexin@gmail.com>
2022-08-17 15:56:31 +0800

    multiprocess echo server

--- 10/echo_mpserver.c
+++ 10/echo_mpserver.c
@@ -29,10 +29,9 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    act.sa_handler = read_childproc;
+    act.sa_handler = read_childproc;  // 处理子进程，防止僵尸进程
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
-
     state = sigaction(SIGCHLD, &act, 0);
 
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
@@ -52,8 +51,7 @@ int main(int argc, char** argv) {
     while(1) {
         addr_size = sizeof(client_addr);
         client_sock = accept(serv_sock, (struct sockaddr*)&client_addr, &addr_size);
-        if(client_sock == -1) {
-            // error_handling("accept() error");
+        if(client_sock == -1) {  // 有一个连接失败不能影响其他的连接
             continue;
         }
         else {
@@ -65,8 +63,8 @@ int main(int argc, char** argv) {
             close(client_sock);
             continue;
         }
-        else if(pid == 0) {
-            close(serv_sock);
+        else if(pid == 0) {  // 子进程
+            close(serv_sock);  // 由于服务器套接字复制给了子进程，所以子进程要销毁
             while((str_len = read(client_sock, buf, BUF_SIZE)) != 0) {
                 write(client_sock, buf, str_len);
             }
@@ -74,8 +72,8 @@ int main(int argc, char** argv) {
             puts("client disconnect!");
             return 0;
         }
-        else {
-            close(client_sock);
+        else {  // 父进程
+            close(client_sock);  // 因为客户端套接字已经复制给了子进程，所以服务器要销毁自己的
         }
 
     }
